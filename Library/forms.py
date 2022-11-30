7514e1b22fc5324949f6f5cda095ee0f9dabc629
Nimesh <nimeshsingh948@gmail.com>
2022-11-30 19:05:10 +0530

    complete

--- Library/forms.py
+++ Library/forms.py
@@ -19,5 +19,8 @@ class IssueBookForm(forms.Form):
 
     class Meta:
         model = models.IssuedBooks
-    isbn_ = forms.ModelChoiceField(queryset=models.Book.objects.all(), empty_label='Title|ISBN', to_field_name='isbn', label='Book Title')
-    user_ = forms.ModelChoiceField(queryset=models.User.objects.filter(is_superuser=False, is_staff=False, is_active=True), empty_label='Name|Email', to_field_name='email', label='Student') 
+    isbn_ = forms.ModelChoiceField(queryset=models.Book.objects.all(),widget=forms.Select(attrs={'class':'form-control'}) , empty_label='Title|ISBN', to_field_name='isbn', label='Book Title' )
+    user_ = forms.ModelChoiceField(queryset=models.User.objects.filter(is_superuser=False, is_staff=False, is_active=True),widget=forms.Select(attrs={'class':'form-control'}) , empty_label='Name|Email', to_field_name='email', label='Student') 
+
+
+    
